ca076190bacad019720407460841a86efb41da0d
minhquan3197 <minhquan.k0r1@gmail.com>
2020-01-19 14:03:04 +0700

    Update

--- js/index.js
+++ js/index.js
@@ -2,16 +2,18 @@ $description = $(".description");
 
 $('path').hover(function () {
     $description.addClass('active');
-    $description.html($(this).attr('title'));
+    if ($(this).attr('images')) {
+        $description.html($(this).attr('title') + ' ' + "<br/><img src='" + $(this).attr('images') + "' width='100px' />");
+    } else {
+        $description.html($(this).attr('title'));
+    }
 }, function () {
     $description.removeClass('active');
 });
 
 $(document).on('mousemove', function (e) {
-
     $description.css({
         left: e.pageX,
         top: e.pageY - 70
     });
-
 });
