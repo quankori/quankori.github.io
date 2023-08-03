550fb8dc95eb4920bfcdef8bc789cd83f9063908
k0r1 <mimnhquan.k0r1@gmail.com>
2023-08-03 21:20:09 +0700

    Update logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,14 +38,12 @@ export default function Home(): JSX.Element {
 
           <section>
             <Typography mt={5} variant="subtitle1" gutterBottom>
-              I have 4 years of experience in coding with technically Node.js,
-              and Go, and have a few technical front-end frameworks such as
-              React.js, Angular, Vue.js and Solidity, Rust for a smart contract,
-              recently I working and researching technical domain blockchain and
-              communication skills. I can work full-stack with front-end,
-              back-end, and a few blockchain but I see currently myself as
-              having a stronger back-end than front-end besides I see myself as
-              strong about research and work with other technologies.
+              I have 5 years of experience in coding with technically Node.js
+              and Go, and have a few technical DevOPS such as AWS, CI/CD,
+              BashScript, and Solidity for a smart contract, recently I working
+              and researching technical cloud engineering and communication
+              skills. I can work back-end, and a few DevOps besides I see myself
+              as strong about research and working with other technologies.
             </Typography>
           </section>
 
@@ -53,67 +51,114 @@ export default function Home(): JSX.Element {
             <Typography mt={5} variant="h5" align="center">
               My Technical
             </Typography>
-            <Grid mt={5} container spacing={2} style={{ position: "relative" }}>
-              <Grid item xs={12} sm={6} className="grid-item">
-                <Typography variant="h6" align="center">
-                  Back-end / DevOPS
-                </Typography>
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    gap: 2,
-                    alignItems: "center",
-                  }}
-                >
-                  {[
-                    "Go",
-                    "Node.js",
-                    "Nest.js",
-                    "BashScript",
-                    "MongoDB",
-                    "MySQL",
-                    "Nginx",
-                    "Docker",
-                    "Truffle",
-                    "Amazon Web Services",
-                    "CI/CD",
-                  ].map((name) => {
-                    return <Chip key={name}>{name}</Chip>;
-                  })}
-                </Box>
+            <Grid mt={10} container spacing={2}>
+              <Grid item xs={1} sm={1} className="grid-item">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+                  width={50}
+                />
+                <p>Node.js</p>
               </Grid>
-              {/* {!isMobile && <div className="magic-border"></div>} */}
-              <Grid item xs={12} sm={6} className="grid-item">
-                <Typography variant="h6" align="center">
-                  Blockchain
-                </Typography>
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    gap: 2,
-                    alignItems: "center",
-                  }}
-                >
-                  {[
-                    "Solidity",
-                    "Tendermint",
-                    "Ethereum Virtual Machine",
-                    "Web3.js",
-                    "Ethers.js",
-                    "IPFS",
-                    "Hardhat",
-                    "Truffle",
-                    "DeFI",
-                    "GameFI",
-                    "Decentralized Exchange",
-                  ].map((name) => {
-                    return <Chip key={name}>{name}</Chip>;
-                  })}
-                </Box>
+              <Grid item xs={1} sm={1} className="grid-item">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain.svg"
+                  width={50}
+                />
+                <p>Nest.js</p>
+              </Grid>
+              <Grid item xs={1} sm={1} className="grid-item">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg"
+                  width={50}
+                />
+                <p>Go</p>
+              </Grid>
+              <Grid item xs={1} sm={1} className="grid-item">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg"
+                  width={50}
+                />
+                <p>Bash</p>
+              </Grid>
+              <Grid item xs={1} sm={1} className="grid-item">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/terraform/terraform-original.svg"
+                  width={50}
+                />
+                <p>Terraform</p>
+              </Grid>
+              <Grid item xs={1} sm={1} className="grid-item">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg"
+                  width={50}
+                />
+                <p>AWS</p>
+              </Grid>
+              <Grid item xs={1} sm={1} className="grid-item">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
+                  width={50}
+                />
+                <p>Mongo</p>
+              </Grid>
+              <Grid item xs={1} sm={1} className="grid-item">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
+                  width={50}
+                />
+                <p>MySQL</p>
+              </Grid>
+              <Grid item xs={1} sm={1} className="grid-item">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+                  width={50}
+                />
+                <p>Docker</p>
+              </Grid>
+              <Grid item xs={1} sm={1} className="grid-item">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg"
+                  width={50}
+                />
+                <p>Nginx</p>
               </Grid>
             </Grid>
+          </section>
+          <section>
+            <Typography mt={5} variant="h5" align="center">
+              Certifications
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                gap: 2,
+                alignItems: "center",
+              }}
+              mt={5}
+              mb={5}
+            >
+              <a
+                href="https://www.credly.com/earner/earned/badge/7f6efa2a-70e0-438b-ba6c-7d6d1c1265c5"
+                target="_blank"
+              >
+                <img
+                  src="https://images.credly.com/images/00634f82-b07f-4bbd-a6bb-53de397fc3a6/image.png"
+                  alt="AWS Certified Cloud Practitioner"
+                  style={{ width: 150, height: 150 }}
+                />
+              </a>
+              <a
+                href="https://www.credly.com/earner/earned/badge/277af70e-f2a4-44ce-b2db-2c861fe408e6"
+                target="_blank"
+              >
+                <img
+                  src="https://images.credly.com/size/340x340/images/0e284c3f-5164-4b21-8660-0d84737941bc/image.png"
+                  alt="AWS Certified Solutions Architect – Associate"
+                  style={{ width: 150, height: 150 }}
+                />
+              </a>
+            </Box>
           </section>
           <section>
             <Typography mt={5} variant="h5" align="center">
@@ -145,32 +190,6 @@ export default function Home(): JSX.Element {
                 </TimelineItem>
               );
             })}
-          </section>
-          <section>
-            <Typography mt={5} variant="h5" align="center">
-              Certifications
-            </Typography>
-            <Box
-              sx={{
-                display: "flex",
-                flexWrap: "wrap",
-                gap: 2,
-                alignItems: "center",
-              }}
-              mt={5}
-              mb={5}
-            >
-              <a
-                href="https://www.credly.com/earner/earned/badge/7f6efa2a-70e0-438b-ba6c-7d6d1c1265c5"
-                target="_blank"
-              >
-                <img
-                  src="https://images.credly.com/images/00634f82-b07f-4bbd-a6bb-53de397fc3a6/image.png"
-                  alt="AWS Certified Cloud Practitioner"
-                  style={{ width: 150, height: 150 }}
-                />
-              </a>
-            </Box>
           </section>
         </div>
       </main>
